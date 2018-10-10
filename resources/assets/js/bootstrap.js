b0180045bdbfb01af554b73f54a2c34989e4b066
vfedorov <fedorovVL@gmail.com>
2018-10-11 00:01:11 +0300

    Update bootstrap.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
-import Form from './services/Form'
+import Form from './services/form'
 
 window.Vue = Vue;
 Vue.use(VueRouter);
